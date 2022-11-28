bc149931999a5d8d16ed2ab713e313175d13bdc4
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-28 23:38:40 +0530

    tailwind css config

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+// Defined themes for tailwindcss
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {},
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
