3b9385112e9a501e50391528bcd67a35d8756c44
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-12-10 23:53:45 +0530

    index of components

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import About from './About'
+import Button from './Button'
+import Creators from './Creators'
+import Footer from './Footer'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import LiveAuction from './LiveAuction'
+import Roadmap from './Roadmap'
+import TopCollection from './TopCollection'
+
+
+export {
+About,
+Button,
+Creators,
+Footer,
+Hero,
+LiveAuction,
+Navbar,
+Roadmap,
+TopCollection
+};
