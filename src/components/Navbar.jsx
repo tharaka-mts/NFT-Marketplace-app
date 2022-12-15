9bcbd64dd64bf9e66175d0942191963115cc215e
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-12-15 23:34:42 +0530

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-black'>
+      <img src={logo} alt="logo" className=' h-[43px] w-[50px] '/>
+    </div>
   )
 }
 
