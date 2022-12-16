13a01fc08a446b0f9c5afb5325d998da8e703f37
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-12-16 22:00:58 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,17 @@ import { logo } from '../assets'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row'>
-      <img src={logo} alt="logo" className=' h-[43px] w-[50px] '/>
+    <div className='flex py-5 items-center'>
+      <div className='flex flex-row items-center'>
+          <img src={logo} alt="logo" className=' h-[43px] w-[50px] mr-4'/>
+          <h1 className='text-[32px] font-bold'>NFT <span className='text-lightBlue'>core</span></h1>
+      </div>
+
+        <ul className='list-none justify-end flex-1'>
+          <li>Discover</li>
+          <li>Marketplace</li>
+          <li>How it Works</li>
+        </ul>
     </div>
   )
 }
