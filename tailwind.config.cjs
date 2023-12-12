e9b13394095ae67f7cd07a4427f67367a6b3519b
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-12 10:18:08 +0530

    tailwind config update fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,21 +6,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': "#080B2A",
-        'lightBlue': "#15BFFD",
-        'dimWhite': "rgba(255, 255, 255, 0.7)",
+        primary: "#080B2A",
+        lightBlue: "#15BFFD",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        clash: ["Clash Display", "sans-serif"],
       },
     },
     screens: {
-      'xs': "480px",
-      'ss': "620px",
-      'sm': "768px",
-      'md': "1060px",
-      'lg': "1200px",
-      'xl': "1700px",
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
