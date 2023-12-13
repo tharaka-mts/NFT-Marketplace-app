f2df927ec207e98764f799fed7963fe617097cd7
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-14 01:13:47 +0530

    button component props update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({className}) => (
-    <button type='button' className={className}>Connect Wallet</button>
-  )
+const Button = ({ className, title, type }) => {
+  const outline =
+    "text-lightBlue border-lightBlue hover:bg-lightBlue hover:text-white hover:border-dimWhite";
+  const solid = "bg-lightBlue text-white border-lightBlue hover:bg-[#2eb2ff]";
+  const buttonType = type === "solid" ? solid : outline;
 
-export default Button
+  return (
+    <button
+      type="button"
+      className={`rounded-full border px-5 py-2 transition-colors duration-[600ms] ease-in-out ${buttonType} ${className}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
