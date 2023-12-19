dc8b06336979b5abb03d0ed2d9de08cff1f9b58b
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-20 01:37:22 +0530

    assets update

--- src/styles.js
+++ src/styles.js
@@ -28,5 +28,3 @@ export const layout = {
 };
 
 export default styles;
-
-
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,13 +14,16 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         clash: ["Clash Display", "sans-serif"],
       },
+      backgroundImage: {
+        rectangle: "url('./src/assets/rectangle.svg')",
+      },
     },
     screens: {
       xs: "480px",
       ss: "620px",
       sm: "768px",
       md: "1060px",
-      lg: "1200px",
+      lg: "1280px",
       xl: "1700px",
     },
   },
