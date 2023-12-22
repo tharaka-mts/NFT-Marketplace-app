b0beb375d54e7c42bf85356bcf58e47135c60000
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-23 03:42:54 +0530

    style: user images

---
+++ src/assets/userImages/index.js
@@ -0,0 +1,6 @@
+export { default as u1 } from "./u1.jpg";
+export { default as u2 } from "./u2.jpg";
+export { default as u3 } from "./u3.jpg";
+export { default as img1 } from "./1.png";
+export { default as img2 } from "./2.png";
+export { default as img3 } from "./3.png";
