92b9cba70061315d89b72d49911588704008a08c
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-23 03:41:59 +0530

    feat - auction list data

---
+++ src/assets/dummy.js
@@ -0,0 +1,25 @@
+import { img1, img2, img3, u1, u2, u3 } from "./userImages";
+
+export const auctionData = [
+  {
+    price: "3.2 ETH",
+    image: img1,
+    userImage: u1,
+    status: "Golden Hour",
+    name: "John Doe",
+  },
+  {
+    price: "2 BTC",
+    image: img2,
+    userImage: u2,
+    status: "Golden Hour",
+    name: "Emily Smith",
+  },
+  {
+    price: "4 BNB",
+    image: img3,
+    userImage: u3,
+    status: "Golden Hour",
+    name: "James Bond",
+  },
+];
