aa21f0ace4956bc2f37f6c81229bfbac80cb4eb7
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-24 23:39:50 +0530

    refactor: card bg rectangle svg -> png

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ export { default as logo } from "./logo.svg";
 export { default as search } from "./search.svg";
 export { default as upload } from "./upload.svg";
 export { default as beatLine } from "./beatLine.svg";
-export { default as rectangle } from "./rectangle.svg";
+export { default as rectangle } from "./rectangle.png";
 export { default as slogan } from "./slogan.svg";
 export { default as vectorLine } from "./vectorLine.svg";
 export { default as vectorRounds } from "./vectorRounds.svg";
