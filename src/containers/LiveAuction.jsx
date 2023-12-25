d22d685a97dec9136d9222be01fa53ff410309ed
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-25 23:40:07 +0530

    style: font weight semi bold in live auction

--- src/containers/LiveAuction.jsx
+++ src/containers/LiveAuction.jsx
@@ -5,13 +5,13 @@ const LiveAuction = () => {
   return (
     <div className="mt-24">
       <div className="text-center">
-        <h1 className="font-clash font-normal text-[32px]"> Live Auction</h1>
+        <h1 className="font-clash font-semibold text-[32px]"> Live Auction</h1>
         <p className="font-inter">
           The largest and unique Super rare NFT marketplace <br />
           For crypto-collectibles
         </p>
       </div>
-      <div className="lg:flex flex-row gap-16">
+      <div className="flex flex-wrap gap-16">
         {auctionData.map((item, index) => (
           <div key={index} className="mt-16 font-inter">
             <div className="bg-gradient-to-br from-indigo-300/20 to-purple-300/10 border-solid border-2 border-violet-400/30 rounded-xl p-5 w-[384px] h-[460px]">
