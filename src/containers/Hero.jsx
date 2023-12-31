07984f7836a7271f268bc09469d2bb6d0881b1f4
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2024-01-01 02:26:10 +0530

    style: hero section mobile responsive

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -3,9 +3,9 @@ import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <div className="flex md:flex-nowrap flex-wrap gap-5 mt-24">
+    <div className="flex md:flex-nowrap flex-wrap gap-5 md:mt-24 mt-12 md:ml-0 ss:ml-[15%] xs:ml-5 lg:px-8 xs:px-6 px-4">
       <div className="flex-1">
-        <h3 className="text-[5rem] font-clash font-[600] leading-[5rem]">
+        <h3 className="ss:text-[5rem] xs:text-[4rem] text-[3rem] font-clash font-[600] leading-[5rem]">
           Super NFT
           <br />
           <span className="font-[400]">Marketplace</span>
@@ -17,7 +17,7 @@ const Hero = () => {
         </p>
         <div className="flex items-center my-6">
           <Button
-            className="w-44 font-medium text-lg"
+            className="w-44 font-medium xs:text-lg text-xs"
             title="Connect Wallet"
             type="solid"
           />
@@ -25,7 +25,7 @@ const Hero = () => {
           <img src={arrow} alt="arrow" className="ml-4" />
         </div>
         <h3 className="text-xl font-medium">Last 7 days popular search</h3>
-        <div className="flex gap-2 mt-5 text-xs">
+        <div className="flex flex-wrap gap-2 mt-5 text-xs">
           <p className="rounded-full bg-lightBlue px-3 py-1">All</p>
           <p className="rounded-full border border-dimWhite px-3 py-1">Music</p>
           <p className="rounded-full border border-dimWhite px-3 py-1">
@@ -33,7 +33,7 @@ const Hero = () => {
           </p>
           <p className="rounded-full border border-dimWhite px-3 py-1">Games</p>
         </div>
-        <div className="flex gap-2 mt-3 text-xs">
+        <div className="flex flex-wrap gap-2 mt-3 text-xs">
           <p className="rounded-full border border-dimWhite px-3 py-1">
             Sports
           </p>
@@ -49,7 +49,11 @@ const Hero = () => {
         </div>
       </div>
       <div className="flex">
-        <img src={rectangleHero} alt="card" />
+        <img
+          src={rectangleHero}
+          alt="card"
+          className="md:scale-100 sm:scale-75 ss:scale-75 xs:scale-[60%] ss:block hidden"
+        />
       </div>
     </div>
   );
